2508d8a3ee5437a17e601055b60126d2c4ad3e15
Abhigyan <singhdefinite12@gmail.com>
2024-07-15 00:01:20 +0530

    custom

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,15 +1,15 @@
 
 function customRender(reactElement, container){
-    const domElement = document.createElement
-    (reactElement.type)
-    domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href',reactElement.props.
-        href)
+    // const domElement = document.createElement
+    // (reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href',reactElement.props.
+    //     href)
 
-        domElement.setAttribute('target',reactElement.props.
-            target)
+    //     domElement.setAttribute('target',reactElement.props.
+    //         target)
 
-       container.appendChild(domElement) 
+    //    container.appendChild(domElement) 
 }
 
 
