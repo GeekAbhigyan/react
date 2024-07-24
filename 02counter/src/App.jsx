f7fcbb1501d07ad9d9b5291b19a7a2cf960a7f1e
Abhigyan <singhdefinite12@gmail.com>
2024-07-25 05:06:52 +0530

    usestate dom updation learnt

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import {useState} from "react";
+
+
+//ui updation ko react control karta hai 
+
+function App() {
+  
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 6;
+
+  const addvalue = function(){
+    
+    // counter = counter +1;
+    setCounter(counter+1);
+    console.log("clicked", counter)
+  }
+
+  const removeValue = ()=>{
+   
+    setCounter(counter-1);
+    console.log("clicked", counter)
+  }
+
+
+
+  return (
+    <>
+     <h1>counter - react </h1>
+     <h2>counter value : {counter}</h2>
+
+     <button onClick={addvalue}>ADD value</button>
+     <br />
+    <button onClick={removeValue}>REMOVE value{counter}</button>
+
+    {/* <p> footer : {counter}</p> */}
+    </>
+  )
+}
+
+export default App
