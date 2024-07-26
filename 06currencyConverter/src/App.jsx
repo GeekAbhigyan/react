9be34f9a378277507414309fe58ef47094c740c6
Abhigyan <singhdefinite12@gmail.com>
2024-07-26 22:15:01 +0530

    currency

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-300 flex text-center '>currency converter</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 06currencyConverter/src/hooks/useCurencyInfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+
+
+
+
+
+function useCurrencyinfo(currency){
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
+        .then((res)=> res.json())
+        .then((res) => setDefaultHighWaterMark(res[currency]))
+        console.log(data);
+    },[])
+
+}
