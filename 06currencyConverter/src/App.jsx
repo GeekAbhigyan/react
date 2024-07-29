ab31abeb4eb5b70947e9e08cad0d8d406968e8b5
Abhigyan <singhdefinite12@gmail.com>
2024-07-29 09:44:17 +0530

    currency converter updated

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import { InputBox} from "./components/InputBox.jsx";
+import {useCurrencyinfo} from "./hooks/useCurencyInfo.js";
+import './App.css'
+
 
 
 
 function App() {
+  const [amount,setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to,setTo] = useState("inr");
+  const [convertedAmount , setConvertedAmount] = useState(0);
+  
   
 
   return (
     <>
-      <h1 className='text-3xl bg-orange-300 flex text-center '>currency converter</h1>
+      <h1 className='text-center text-3xl bg-orange-300 flex'>currency converter</h1>
     </>
   )
 }
--- 06currencyConverter/src/hooks/useCurencyInfo.js
+++ 06currencyConverter/src/hooks/useCurencyInfo.js
@@ -6,11 +6,16 @@ import { useEffect, useState } from "react";
 
 
 function useCurrencyinfo(currency){
+    const [data,setData] = useState({})
     useEffect(()=>{
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
         .then((res)=> res.json())
         .then((res) => setDefaultHighWaterMark(res[currency]))
         console.log(data);
-    },[])
+    },[currency,])
+
+    return data
 
 }
+
+export default useCurrencyinfo
